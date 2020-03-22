71b79cad0c121d1bd0c8025ce46a05259a536fd3
sands31 <49200520+sands31@users.noreply.github.com>
2020-03-22 19:29:09 +0100

    vue-router added

--- cafe-with-a-vue/cli-version/src/App.vue
+++ cafe-with-a-vue/cli-version/src/App.vue
@@ -1,115 +1,32 @@
 <template>
-  <div id="app" class="app">
-    <h1>{{ restaurantName }}</h1>
-    <p class="description">
-      Welcome to {{ restaurantName }}! We are known for our freshly baked bread
-      and french pastries! Give you morning a warm start or treat yourself in
-      the middle of the day. Our butter is imported from local farmers in
-      France. Once you take your first bite, you will see why everyone can't get
-      enough!
-    </p>
-
-    <section class="menu">
-      <h2>Menu</h2>
-      <MenuItem
-        v-for="item in simpleMenu"
-        :addToShoppingCart="addToShoppingCart"
-        :name="item.name"
-        :image="item.image"
-        :quantity="item.quantity"
-        :inStock="item.inStock"
-        :key="item.name"
-      />
-    </section>
-
-    <aside class="shopping-cart">
-      <h2>Shopping Cart: {{ shoppingCart }} items</h2>
-    </aside>
-
-    <h2>Contact Us</h2>
-    <p>Address: {{ address }}</p>
-    <p>Phone: {{ phone }}</p>
-    <p>Email: {{ email }}</p>
-    <p>Hours:</p>
-    <ul>
-      <li>M-F: 06:00 to 16:00</li>
-      <li>Saturday: 07:00 to 14:00</li>
-      <li>Sunday: 07:00 to 12:00</li>
-    </ul>
-    <footer class="footer">
-      <p>{{ copyright }}</p>
-    </footer>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import MenuItem from './components/MenuItem'
-
-export default {
-  name: 'App',
-  components: {
-    MenuItem
-  },
-  data() {
-    return {
-      address: '9274 Beurre Ave, Paris, France',
-      email: 'hello@cafewithavue.bakery',
-      phone: '+08 88 88 88 88 88',
-      restaurantName: 'Cafe with A Vue',
-      shoppingCart: 0,
-      simpleMenu: [
-        {
-          name: 'Crossiant',
-          image: {
-            source: '/images/crossiant.jpg',
-            alt: 'A crossiant'
-          },
-          inStock: true,
-          quantity: 1
-        },
-        {
-          name: 'French Baguette',
-          image: {
-            source: '/images/french-baguette.jpeg',
-            alt: 'Four French Baguettes'
-          },
-          inStock: true,
-          quantity: 1
-        },
-        {
-          name: 'Éclair',
-          image: {
-            source: '/images/eclair.jpg',
-            alt: 'Chocolate Éclair'
-          },
-          inStock: false,
-          quantity: 1
-        }
-      ]
-    }
-  },
-  computed: {
-    copyright() {
-      const currentYear = new Date().getFullYear()
-
-      return `Copyright ${this.restaurantName} ${currentYear}`
-    }
-  },
-  methods: {
-    addToShoppingCart(amount) {
-      this.shoppingCart += amount
-    }
-  }
-}
-</script>
-
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
