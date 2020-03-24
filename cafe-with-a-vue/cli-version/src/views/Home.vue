a367387888e38c7d956d324bec4cca8049b6a8a4
sands31 <49200520+sands31@users.noreply.github.com>
2020-03-24 16:13:11 +0100

    before enabling histor mode for vue-router

--- cafe-with-a-vue/cli-version/src/views/Home.vue
+++ cafe-with-a-vue/cli-version/src/views/Home.vue
@@ -1,18 +1,102 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="app">
+    <h1>{{ restaurantName }}</h1>
+    <p class="description">
+      Welcome to {{ restaurantName }}! We are known for our freshly baked bread
+      and french pastries! Give you morning a warm start or treat yourself in
+      the middle of the day. Our butter is imported from local farmers in
+      France. Once you take your first bite, you will see why everyone can't get
+      enough!
+    </p>
+
+    <section class="menu">
+      <h2>Menu</h2>
+      <MenuItem
+        v-for="item in simpleMenu"
+        :addToShoppingCart="addToShoppingCart"
+        :name="item.name"
+        :image="item.image"
+        :quantity="item.quantity"
+        :inStock="item.inStock"
+        :key="item.name"
+      />
+    </section>
+
+    <aside class="shopping-cart">
+      <h2>Shopping Cart: {{ shoppingCart }} items</h2>
+    </aside>
+
+    <footer class="footer">
+      <p>{{ copyright }}</p>
+    </footer>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import MenuItem from '@/components/MenuItem'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    MenuItem
+  },
+  data() {
+    return {
+      restaurantName: 'Cafe with A Vue',
+      shoppingCart: 0,
+      simpleMenu: [
+        {
+          name: 'Crossiant',
+          image: {
+            source: '/images/crossiant.jpg',
+            alt: 'A crossiant'
+          },
+          inStock: true,
+          quantity: 1
+        },
+        {
+          name: 'French Baguette',
+          image: {
+            source: '/images/french-baguette.jpeg',
+            alt: 'Four French Baguettes'
+          },
+          inStock: true,
+          quantity: 1
+        },
+        {
+          name: 'Éclair',
+          image: {
+            source: '/images/eclair.jpg',
+            alt: 'Chocolate Éclair'
+          },
+          inStock: false,
+          quantity: 1
+        }
+      ]
+    }
+  },
+  computed: {
+    copyright() {
+      const currentYear = new Date().getFullYear()
+
+      return `Copyright ${this.restaurantName} ${currentYear}`
+    }
+  },
+  methods: {
+    addToShoppingCart(amount) {
+      this.shoppingCart += amount
+    }
   }
 }
 </script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
