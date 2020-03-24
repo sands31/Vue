b91c22f25c68b933f61eeb81392fb967a5384334
sands31 <49200520+sands31@users.noreply.github.com>
2020-03-24 17:22:32 +0100

    fix conflict

--- cafe-with-a-vue/cli-version/src/components/MenuItem.vue
+++ cafe-with-a-vue/cli-version/src/components/MenuItem.vue
@@ -27,29 +27,18 @@ export default {
 </script>
 
 <template>
-<<<<<<< HEAD
-  <div class="menu-item">
-    <img class="menu-item__image" :src="image.source" :alt="image.alt" />
-=======
-  <div>
+  <div >
     <h3>{{ name }}</h3>
     <img :src="image.source" :alt="image.alt" />
     <p>Price: {{ generatedPrice }} <span v-if="onSale">(10% off!)</span></p>
     <p v-if="inStock">In Stock</p>
     <p v-else>Out of Stock</p>
->>>>>>> feature: Add onSale text
     <div>
-      <h3>{{ name }}</h3>
-      <p>Price: {{ generatedPrice }}</p>
-      <p v-if="inStock">In Stock</p>
-      <p v-else>Out of Stock</p>
-      <div>
-        <label for="add-item-quantity">Quantity: {{ quantity }}</label>
-        <input v-model.number="quantity" id="add-item-quantity" type="number" />
-        <button @click="addToShoppingCart(quantity)">
-          Add to Shopping Cart
-        </button>
-      </div>
+      <label for="add-item-quantity">Quantity: {{ quantity }}</label>
+      <input v-model.number="quantity" id="add-item-quantity" type="number" />
+      <button @click="addToShoppingCart(quantity)">
+        Add to Shopping Cart
+      </button>
     </div>
   </div>
 </template>
