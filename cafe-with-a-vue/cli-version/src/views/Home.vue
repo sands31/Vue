470006fe517eb2822384999efb12cabc25e00e7c
sands31 <49200520+sands31@users.noreply.github.com>
2020-03-26 15:46:59 +0100

    first solution

--- cafe-with-a-vue/cli-version/src/views/Home.vue
+++ cafe-with-a-vue/cli-version/src/views/Home.vue
@@ -35,6 +35,7 @@
 
 <script>
 import MenuItem from '../components/MenuItem'
+import { mapState, mapGetters } from 'vuex'
 
 export default {
   name: 'Home',
@@ -43,52 +44,15 @@ export default {
   },
   data() {
     return {
-      restaurantName: 'Cafe with A Vue',
-      shoppingCart: 0,
-      simpleMenu: [
-        {
-          name: 'Crossiant',
-          image: {
-            source: '/images/crossiant.jpg',
-            alt: 'A crossiant'
-          },
-          inStock: true,
-          quantity: 1,
-          price: 2.99
-        },
-        {
-          name: 'French Baguette',
-          image: {
-            source: '/images/french-baguette.jpeg',
-            alt: 'Four French Baguettes'
-          },
-          inStock: true,
-          quantity: 1,
-          price: 3.99
-        },
-        {
-          name: 'Éclair',
-          image: {
-            source: '/images/eclair.jpg',
-            alt: 'Chocolate Éclair'
-          },
-          inStock: false,
-          quantity: 1,
-          price: 4.99
-        }
-      ]
     }
   },
   computed: {
-    copyright() {
-      const currentYear = new Date().getFullYear()
-
-      return `Copyright ${this.restaurantName} ${currentYear}`
-    }
+    ...mapState (['restaurantName', 'shoppingCart', 'simpleMenu']),
+    ...mapGetters (['copyright()'])
   },
   methods: {
     addToShoppingCart(amount) {
-      this.shoppingCart += amount
+      this.$store.state.shoppingCart += amount
     }
   }
 }
